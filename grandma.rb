ec7af37d7c78ad81b2de6142abe36755205535df
Kurtis Leach <danceflurry4366@gmail.com>
2019-05-08 22:50:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
-  elsif phrase == phrase.lowercase
+  else phrase == phrase.lowercase
     "HUH?! SPEAK UP, SONNY!"
   end
 end
